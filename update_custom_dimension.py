d2bab7dcc247483ec6d763973e94d299ddc53f12
Justin Ryburn <jryburn@kentik.com>
2026-07-13 10:54:13 -0500

    Clean up lint errors

--- update_custom_dimension.py
+++ update_custom_dimension.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
+"""Update Kentik custom dimension populators from UniFi client data."""
 #
-#   update_custom_dimension.py  written by Justin Ryburn (jryburn@kentik.com) 2022 Apr 1
+#   update_custom_dimension.py  written by Justin Ryburn (jryburn@kentik.com)
+#   2022 Apr 1
 #
-#   This simple python script grabs a client list from a Ubiquiti Unifi controller and saves them to a .csv file.
-#  It then uses the Kentik bulk API to quickly load custom dimension populator data.
+#   This simple python script grabs a client list from a Ubiquiti Unifi
+#   controller and saves them to a .csv file.
 #
-#   This script uses the Kentik bulk API written by Blake Caldwell which can be located at:
+#   It then uses the Kentik bulk API to quickly load custom dimension populator
+#   data.
+#
+#   This script uses the Kentik bulk API written by Blake Caldwell which can
+#   be located at:
 #   https://github.com/kentik/kentikapi-py/tree/master/kentikapi/v5
 
-from unificontrol import UnifiClient
-from kentikapi.v5 import tagging
-from decouple import config
 from collections import defaultdict
 import csv
 import time
 import ssl
+from unificontrol import UnifiClient
+from kentikapi.v5 import tagging
+from decouple import config
 
 ################################
 # This section are the user editable variables the script relies upon
@@ -22,27 +28,31 @@ import ssl
 # The config function pulls these in to the script.
 UNIFI_USER = config('UNIFI_USER')
 UNIFI_PASSWORD = config('UNIFI_PASSWORD')
-UNIFI_SITE = 'default'
-UNIFI_HOST = 'docklands.ryburn.org'
-cert = ssl.get_server_certificate((UNIFI_HOST, 8443))
+UNIFI_SITE = 'jryburn-fw'
+# UNIFI_HOST = 'docklands.ryburn.org'
+UNIFI_HOST = '192.168.0.1'
+CERT = ssl.get_server_certificate((UNIFI_HOST, 8443))
 # Name of the CSV file we are going to store the client list in
-csvfile = 'clients.csv'
+CSV_FILE = 'clients.csv'
 
 ################################
 # Kentik options:
 option_api_email = config('KENTIK_API_EMAIL')
 option_api_token = config('KENTIK_API_TOKEN')
 #
-# "populator_name" and "populator_data" should be the column headers for the CSV file.
+# "POPULATOR_NAME" and "POPULATOR_DATA" should be the column headers for the
+# CSV file.
 #
-populator_name = "hostname"
-populator_data = "mac_addr"
-# This script automatically creates both the source and destination populators for a given custom dimension.
-# In the Kentik portal, you need to create two custom dimensions. The database fields must be in the format of
-# "c_dst_dimension" and "c_src_dimension". The script will add the populators and MAC address to both the source
-# and destination dimensions.
-src_custom_dimension = 'c_src_dyn_hostname'
-dst_custom_dimension = 'c_dst_dyn_hostname'
+POPULATOR_NAME = "hostname"
+POPULATOR_DATA = "mac_addr"
+# This script automatically creates both the source and destination populators
+# for a given custom dimension.
+# In the Kentik portal, you need to create two custom dimensions. The database
+# fields must be in the format of
+# "c_dst_dimension" and "c_src_dimension". The script will add the populators
+# and MAC address to both the source and destination dimensions.
+SRC_CUSTOM_DIMENSION = 'c_src_dyn_hostname'
+DST_CUSTOM_DIMENSION = 'c_dst_dyn_hostname'
 #
 #  End of user variables
 ################################
@@ -51,6 +61,7 @@ tags = defaultdict(list)
 
 
 def pull_clients():
+    """Pull client records from the UniFi controller and write them to CSV."""
     print("Grabbing client list from controller...")
     client = UnifiClient(
         host=UNIFI_HOST,
@@ -58,49 +69,51 @@ def pull_clients():
         username=UNIFI_USER,
         password=UNIFI_PASSWORD,
         site=UNIFI_SITE,
-        cert=cert
+        cert=CERT
         )
     client_list = client.list_clients()
 
     print("Writing client list to a file...")
-    outfile = open(csvfile, "w")
-    # write a header row for the CSV file
-    outfile.write("mac_addr,hostname\n")
-    for client in client_list:
-        mac_addr = client['mac']
-        if 'name' in client:
-            hostname = client['name']
-        elif 'hostname' in client:
-            hostname = client['hostname']
-        else:
-            hostname = client['oui']
-        outfile.write(mac_addr + ',' + hostname + '\n')
-    outfile.close()
+    with open(CSV_FILE, "w", encoding="utf-8") as outfile:
+        # write a header row for the CSV file
+        outfile.write("mac_addr,hostname\n")
+        for client in client_list:
+            mac_addr = client['mac']
+            if 'name' in client:
+                hostname = client['name']
+            elif 'hostname' in client:
+                hostname = client['hostname']
+            else:
+                hostname = client['oui']
+            outfile.write(mac_addr + ',' + hostname + '\n')
 
 
 def push_to_kentik():
+    """Read the CSV and submit custom dimension populators to Kentik."""
     # initialize a batch that will replace all populators
     batch_src = tagging.Batch(True)
     batch_dst = tagging.Batch(True)
 
-    # Use the CSV reader library to read in the CSV file and send the data to the batch API
-    # The column headings should be labeled mac_addr and hostname
+    # Use the CSV reader library to read in the CSV file and send the data to
+    # the batch API. The column headings should be labeled mac_addr and
+    # hostname
 
     print("Reading CSV file...")
-    with open(csvfile, mode='r', encoding='utf-8-sig') as csv_file:
+    with open(CSV_FILE, mode='r', encoding='utf-8-sig') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            populator = row[populator_name]
-            mac = row[populator_data]
+            populator = row[POPULATOR_NAME]
+            mac = row[POPULATOR_DATA]
             tags[populator].append(mac)
     # print(tags)
     # Now iterate through the tags and add them to the batch. We're creating
-    # two batches (for src and dst) so we need two criteria, addresses and populators)
+    # two batches (for src and dst) so we need two criteria, addresses and
+    # populators)
     print("Building batch for upload...")
     for item in tags.items():
         populator = item[0]
         mac = item[1][0]
-        print("Adding populator %s with a mac address value of %s" % (populator, mac))
+        print(f"Adding populator {populator} with mac {mac}")
         crit_dst = tagging.Criteria("dst")
         crit_src = tagging.Criteria("src")
         crit_dst.add_mac_address(mac)
@@ -109,24 +122,25 @@ def push_to_kentik():
         batch_src.add_upsert(populator, crit_src)
 
     # -----
-    # Showtime! Submit the batch as populators for the configured custom dimension
-    # - library will take care of chunking the requests into smaller HTTP payloads
+    # Showtime! Submit the batch as populators for the configured custom
+    # dimension - library will take care of chunking the requests into smaller
+    # HTTP payloads
     # -----
     print("Submitting batch now...")
     client = tagging.Client(option_api_email, option_api_token)
-    guid_dst = client.submit_populator_batch(dst_custom_dimension, batch_dst)
-    guid_src = client.submit_populator_batch(src_custom_dimension, batch_src)
+    guid_dst = client.submit_populator_batch(DST_CUSTOM_DIMENSION, batch_dst)
+    guid_src = client.submit_populator_batch(SRC_CUSTOM_DIMENSION, batch_src)
 
-    #  Wait and display the results
-    for x in range(1, 12):
+    #  Wait and display the dst results
+    for _ in range(1, 12):
         time.sleep(5)
         status = client.fetch_batch_status(guid_dst)
         if status.is_finished():
             print(status.pretty_response())
             break
 
-    #  Wait and display the results
-    for x in range(1, 12):
+    #  Wait and display the src results
+    for _ in range(1, 12):
         time.sleep(5)
         status = client.fetch_batch_status(guid_src)
         if status.is_finished():
